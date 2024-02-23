51c7612bc39e3e2d07a388f92cc59c462842d0c5
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-23 21:38:01 +0900

    DBで使用する項目の型定義した

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+export type TransactionType = "income" | "expense";
+export type incomeCategory = "給料" | "副収入" | "お小遣い";
+export type expenseCategory =
+  | "食費"
+  | "日用品"
+  | "住居費"
+  | "交際費"
+  | "趣味"
+  | "旅費"
+  | "勉強"
+  | "教育費"
+  | "保健"
+  | "医療費"
+  | "ペット";
+
+export interface Transaction {
+  id: string;
+  date: string;
+  amount: number;
+  content: string;
+  type: TransactionType;
+  category: incomeCategory | expenseCategory;
+}
