746c454c19ea8fca63daa4502d58a3a01a4f36a0
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-23 22:17:12 +0900

    firestoreからTransactionデータを取得するAPIを実装し、コンソールで確認した

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,33 @@ import AppLayout from './pages/components/layout/AppLayout';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './theme/theme';
 import { CssBaseline } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Transaction } from "./types/index";
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from "./firebase";
 
 function App() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"))
+
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction // 型アサーションでエラー解消
+        });
+        console.log(transactionsData)
+        setTransactions(transactionsData);
+      } catch (error) {
+
+      }
+    }
+    fetchTransactions();
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
