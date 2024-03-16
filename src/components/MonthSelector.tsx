43aaec2e74f4bf592a890a473f638a25e76881a4
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-16 21:40:38 +0900

    MUIを利用してreportページの日付選択部分を実装した

--- src/components/MonthSelector.tsx
+++ src/components/MonthSelector.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { Box, Button } from '@mui/material'
+import { LocalizationProvider } from '@mui/x-date-pickers'
+// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
 
 const MonthSelector = () => {
     return (
-        <div>
-            MonthSelector
-        </div>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                <Button color={"error"} variant={"contained"}>先月</Button>
+                <div><DatePicker sx={{ mx: 2 }} /></div>
+                <Button color={"primary"} variant={"contained"}>次月</Button>
+            </Box>
+        </LocalizationProvider>
     )
 }
 
