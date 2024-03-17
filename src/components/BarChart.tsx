67d3578582d42bd210cc0d8a24b7e1b2a49c0927
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-03-17 18:17:14 +0900

    react-chartjs-2を使用して収支を表示する棒グラフを実装した

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import { Bar } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 const BarChart = () => {
-  return (
-    <div>
-      BarChart
-    </div>
-  )
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart',
+            },
+        },
+    };
+    const labels = ['2024-01-10', '2024-01-10', '2024-01-10', '2024-01-10', '2024-01-10', '2024-01-10', '2024-01-10'];
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: '収入',
+                data: [100, 200, 300, 400, 500, 600, 700],
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: '支出',
+                data: [100, 200, 300, 400, 500, 600, 700],
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    };
+
+    return <Bar options={options} data={data} />;
+
 }
 
 export default BarChart
